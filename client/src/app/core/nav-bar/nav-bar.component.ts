e0c9b0593e655812d68c636c3cd85a65775781f2
Rijad Džanko <rijad.dzanko@edu.fit.ba>
2021-03-16 13:46:52 +0100

    Admin init

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -8,21 +8,26 @@ import { IUser } from 'src/app/shared/models/user';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
   basket$: Observable<IBasket>;
   currentUser$: Observable<IUser>;
+  isAdmin$: Observable<boolean>;
 
-  constructor(private basketService: BasketService, private accountService: AccountService) { }
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
     this.currentUser$ = this.accountService.currentUser$;
+    this.isAdmin$ = this.accountService.isAdmin$;
   }
 
+  // tslint:disable-next-line: typedef
   logout() {
     this.accountService.logout();
   }
-
 }
