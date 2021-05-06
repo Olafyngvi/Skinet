e98805dcee6f8e4bc5be3621cf6898f0d46f4b29
Rijad Dzanko <rijadda@gmail.com>
2021-05-06 15:07:27 +0200

    search section

--- client/src/app/admin/edit-product-form/edit-product-form.component.ts
+++ client/src/app/admin/edit-product-form/edit-product-form.component.ts
@@ -17,6 +17,16 @@ export class EditProductFormComponent implements OnInit {
   @Input() product: ProductFormValues;
   @Input() brands: IBrand[];
   @Input() types: IType[];
+  public fontFamily: object = {
+    items: [
+      {text: 'Segoe UI', value: 'Segoe UI', class: 'e-segoe-ui',  command: 'Font', subCommand: 'FontName'},
+      {text: 'Roboto', value: 'Roboto',  command: 'Font', subCommand: 'FontName'}, // here font is added
+      {text: 'Great vibes', value: 'Great Vibes,cursive',  command: 'Font', subCommand: 'FontName'}, // here font is added
+      {text: 'Noto Sans', value: 'Noto Sans',  command: 'Font', subCommand: 'FontName'},
+      {text: 'Impact', value: 'Impact,Charcoal,sans-serif', class: 'e-impact', command: 'Font', subCommand: 'FontName'},
+      {text: 'Tahoma', value: 'Tahoma,Geneva,sans-serif', class: 'e-tahoma', command: 'Font', subCommand: 'FontName'},
+    ]
+  };
   public tools: object = {
     items: ['Undo', 'Redo', '|',
         'Bold', 'Italic', 'Underline', 'StrikeThrough', '|',
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SearchComponent],
   imports: [
     CommonModule,
     SharedModule
