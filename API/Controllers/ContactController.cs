7cb0367bdfa0adeb5f3f0e95fbfff0f21e1d05f7
Rijad Dzanko <rijadda@gmail.com>
2021-05-12 12:49:52 +0200

    fix

--- API/Controllers/ContactController.cs
+++ API/Controllers/ContactController.cs
@@ -4,26 +4,45 @@ using System.Net.Mail;
 using System.Threading.Tasks;
 using Core.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace API.Controllers
 {
     public class ContactController : BaseApiController
     {
-        public ContactController()
+        private readonly string mailFrom;
+        private readonly string mailFromPassword;
+        private readonly string mailTo;
+        public ContactController(IConfiguration config)
         {
+            mailFrom = config.GetSection("Mail:mailFrom").Value;
+            mailFromPassword = config.GetSection("Mail:fromPassword").Value;
+            mailTo = config.GetSection("Mail:receiverMail").Value;
         }
 
         [HttpPost]
         public void GetMessage(Contact contact)
         {
-            string adresa1 = "rijadda@gmail.com";
+            sendMail(mailFrom, mailFromPassword, mailTo, contact);
+        }
+
+        private void sendMail(string senderMail, string senderPassword, string receiverMail, Contact contact)
+        {
+            string adresa1 = senderMail;
 
 
             var fromAddress = new MailAddress(adresa1, "Drinex Computers");
-            var toAddress = new MailAddress("richie_dzr@hotmail.com", "Poštovani");
-            const string fromPassword = "Greendevil123";
+            var toAddress = new MailAddress(receiverMail, "Poštovani");
+            string fromPassword = senderPassword;
             const string subject = "UPIT";
-            string body = "Poštovani, \n" + contact.Message + "\nLijep pozdrav";
+            string body = contact.Mail
+                          + "\n" + 
+                          contact.Name +
+                          "\n" +
+                          contact.PhoneNumber +
+                          "Poštovani, \n" + 
+                          contact.Message
+                           + "\nLijep pozdrav";
 
             var smtp = new SmtpClient
             {
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -5,22 +5,19 @@ import { SharedModule } from '../shared/shared.module';
 import { SearchComponent } from './search/search.component';
 import { IzdvojenoComponent } from './izdvojeno/izdvojeno.component';
 import { CallactionComponent } from './callaction/callaction.component';
-<<<<<<< HEAD
 import { NovoComponent } from './novo/novo.component';
-=======
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
->>>>>>> 55112e1e256c8da78df613986edfd30a0cd567b0
-
 @NgModule({
-  declarations: [HomeComponent, SearchComponent, IzdvojenoComponent, CallactionComponent, NovoComponent],
-  imports: [
-    CommonModule,
-    SharedModule,
-    BrowserModule,
-    RouterModule
+  declarations: [
+    HomeComponent,
+    SearchComponent,
+    IzdvojenoComponent,
+    CallactionComponent,
+    NovoComponent,
   ],
-  exports: [HomeComponent]
+  imports: [CommonModule, SharedModule, BrowserModule, RouterModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
