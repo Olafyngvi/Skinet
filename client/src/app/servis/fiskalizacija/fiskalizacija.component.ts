1dddb2e47ab4eb0899e782d916c22d81b20275a3
JJayJJay <dzejna.velic@edu.fit.ba>
2021-06-04 15:11:03 +0200

    servis

---
+++ client/src/app/servis/fiskalizacija/fiskalizacija.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fiskalizacija',
+  templateUrl: './fiskalizacija.component.html',
+  styleUrls: ['./fiskalizacija.component.scss']
+})
+export class FiskalizacijaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
